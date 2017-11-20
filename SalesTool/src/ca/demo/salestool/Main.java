84cc36e27ad9434361c7f2eb7aa7ce7402b487f4
sivasai <sivasai746@gmail.com>
2017-11-20 13:46:23 -0600

    Fixed Case on initial greeting display

--- SalesTool/src/ca/demo/salestool/Main.java
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -10,8 +10,9 @@ public class Main {
 
 	}
 	private static void displayGreetings() {
-		System.out.println("HELLO HAPPY SALES PEOPLE");
-		System.out.println("THIS APP SHOWS SALES DATA");
+		System.out.println("Hello Happy Sales People!");
+		System.out.println("This App Shows Sales Data.");
+		System.out.println("--------------------------");
 		System.out.println("Test1");
 		System.out.println("Test2");
 	}
